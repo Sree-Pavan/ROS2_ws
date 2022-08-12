c6e87116070c0595f754f4c4a8d9fa011d470dcd
Sree Pavan Goli <pavanor21@gmail.com>
2022-08-12 15:30:29 +0000

    First commit

---
+++ build/CMakeFiles/Dev.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Dev.dir/Dev.cpp.o"
+  "CMakeFiles/Dev.dir/Dev.cpp.o.d"
+  "libDev.a"
+  "libDev.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Dev.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
